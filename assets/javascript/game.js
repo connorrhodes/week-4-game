b88d93008f8fbefa3460d47fcd847b1874428e80
crhodes <connorrhodes1@yahoo.com>
2017-11-30 21:22:19 -0800

    more changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,56 @@
 // When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 // The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 
+var randomScore;
+var wins;
+var losses;
+var yourScore;
+var gemOne;
+var gemTwo;
+var gemThree;
+var gemFour;
+
+$(document).ready(function () {
+    $(".default").on("click"), function () {
+        $(this).val(crystalHiddenValue) += $("#scoreboard").val();
+    }
+
+});
+
+var cArr = [{ value: 0, img: "assets/images/green_gem.png" },
+{ value: 0, img: "assets/images/yellow_gem.png" },
+{ value: 0, img: "assets/images/red_gem.png" },
+{ value: 0, img: "assets/images/blue_gem.png" }];
+
+
+
+
 //functions
+function initialize() {
+    randomScore = genRandomNumReturn();
+    wins = "";
+    losses = "";
+    yourScore = "";
+
+    for(var i =0; i < cArr.length; i++){
+        cArr[i].value = crystalHiddenValue(1,12);
+        console.log("Value: " + cArr[i].value + cArr[i].img);
+    }
+    // $("#button-1").val(gemOne);
+    // $("#button-2").val(gemTwo);
+    // $("#button-3").val(gemThree);
+    // $("#button-4").val(gemFour);
+    // gemOne = crystalHiddenValue();
+    // gemTwo = crystalHiddenValue();
+    // gemThree = crystalHiddenValue();
+    // gemFour = crystalHiddenValue();
+    // console.log("Button 1: " + gemOne);
+    // console.log("Button 2: " + gemTwo);
+    // console.log("Button 3: " + gemThree);
+    // console.log("Button 4: " + gemFour);
+}
+
+
 function genRandonNum() {
     //random number generated between 19-120
     var randomNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
@@ -27,7 +76,8 @@ function genRandomNumReturn() {
 
 function crystalHiddenValue() {
     //function established to generate a random number between 1-12
-    Math.floor(Math.random() * 12) + 1;
+    return Math.floor(Math.random() * 12) + 1;
+
 }
 
 
@@ -35,6 +85,7 @@ function crystalHiddenValue() {
 //---------------------------------------------------------------------------------------
 //calls
 genRandomNumReturn();
+initialize();
 
 var imgOne = genRandomNumReturn();
 var imgTwo = genRandomNumReturn();
